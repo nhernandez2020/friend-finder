bb84827d143fe5982b24648b5ada524adfe20bf1
Noe Hernandez <Nhernandez2020@hotmail.com>
2020-04-30 12:19:02 -0700

    added friend.js

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # friend-finder
+
+In this application you can take a survey  of 10 question and it will connect you to a friend that you connect with. 
---
+++ data/friends.js
@@ -0,0 +1,164 @@
+var friends = [{
+        "name": "Jose",
+        "photo": "../public/assets/download (4).jpeg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Juana",
+        "photo": "../public/assets/download (1).jpeg",
+        "scores": [
+            5,
+            4,
+            3,
+            5,
+            3,
+            2,
+            1,
+            5,
+            4,
+            3
+        ]
+    },
+    {
+        "name": "julie",
+        "photo": "../public/assets/download (2).jpeg",
+        "scores": [
+            1,
+            1,
+            2,
+            2,
+            3,
+            3,
+            1,
+            3,
+            4,
+            5
+        ]
+    },
+    {
+        "name": "diana",
+        "photo": "../public/assets/download (3).jpeg",
+        "scores": [
+            4,
+            3,
+            3,
+            3,
+            2,
+            1,
+            1,
+            1,
+            2,
+            1
+        ]
+    },
+    {
+        "name": "miguel",
+        "photo": "../public/assets/download.jpeg",
+        "scores": [
+            2,
+            5,
+            5,
+            5,
+            4,
+            3,
+            5,
+            3,
+            4,
+            5
+        ]
+    },
+    {
+        "name": "maribel",
+        "photo": "../public/assets/download (5).jpeg",
+        "scores": [
+            1,
+            1,
+            1,
+            1,
+            1,
+            3,
+            1,
+            2,
+            3,
+            1
+        ]
+    },
+    {
+        "name": "gary",
+        "photo": "../public/assets/images (1).jpeg",
+        "scores": [
+            3,
+            3,
+            2,
+            2,
+            1,
+            1,
+            2,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        "name": "david",
+        "photo": "../public/assets/images (2).jpeg",
+        "scores": [
+            2,
+            4,
+            4,
+            2,
+            1,
+            1,
+            2,
+            5,
+            1,
+            1
+        ]
+    },
+    {
+        "name": "rebecca",
+        "photo": "../public/assets/images.jpeg",
+        "scores": [
+            5,
+            5,
+            4,
+            4,
+            4,
+            4,
+            2,
+            2,
+            1,
+            1
+        ]
+    },
+    {
+        "name": "edwardo",
+        "photo": "../public/assets/IMG_1548-640x668.jpg",
+        "scores": [
+            3,
+            3,
+            3,
+            1,
+            1,
+            1,
+            1,
+            3,
+            2,
+            3
+        ]
+    }
+
+];
+
+module.exports = friends;
